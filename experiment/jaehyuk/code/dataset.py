f2923b5695e67ee524d887c52c3d70750f4bcc64
opijae <0206cjh@daum.net>
2021-05-03 17:22:45 +0000

    cutmix copy&paste

--- experiment/jaehyuk/code/dataset.py
+++ experiment/jaehyuk/code/dataset.py
@@ -3,12 +3,13 @@ import random
 import time
 import json
 import warnings
+from albumentations.augmentations.transforms import ColorJitter, Flip, GaussianBlur
 
 warnings.filterwarnings("ignore")
 
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader,SubsetRandomSampler
 from utils import label_accuracy_score
 import cv2
 
@@ -26,6 +27,8 @@ from albumentations.pytorch import ToTensorV2
 # 시각화를 위한 라이브러리
 import matplotlib.pyplot as plt
 import seaborn as sns
+from copy_paste import copy_paste_class
+from copy_paste import CopyPaste
 
 sns.set()
 
@@ -39,18 +42,62 @@ dataset_path = "/opt/ml/input/data"
 train_path = dataset_path + "/train.json"
 val_path = dataset_path + "/val.json"
 test_path = dataset_path + "/test.json"
+all_path = dataset_path + "/train_all.json"
+class Lighting(object):
+    """Lighting noise(AlexNet - style PCA - based noise)"""
 
+    def __init__(self, alphastd, eigval, eigvec):
+        self.alphastd = alphastd
+        self.eigval = torch.Tensor(eigval)
+        self.eigvec = torch.Tensor(eigvec)
 
+    def __call__(self, img):
+        if self.alphastd == 0:
+            return img
+
+        alpha = img.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(img).clone() \
+            .mul(alpha.view(1, 3).expand(3, 3)) \
+            .mul(self.eigval.view(1, 3).expand(3, 3)) \
+            .sum(1).squeeze()
+
+        return img.add(rgb.view(3, 1, 1).expand_as(img))
+# augmentations
+_train_transform = [
+    # A.Resize(256, 256),
+    # A.RandomResizedCrop(256,256),
+    # A.Flip(),
+    # A.GaussianBlur(),
+    # transforms.RandomHorizontalFlip(),
+    # transforms.ToTensor(),
+    # A.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+    # Lighting,
+    CopyPaste(blend=True, sigma=1, pct_objects_paste=0.5, p=1),  # copy & paste
+    A.Normalize(mean=(0.4611, 0.4403, 0.4193), std=(0.2107, 0.2074, 0.2157)),
+    ToTensorV2(),
+]
+
+_valid_transform = [
+    A.Resize(256, 256),
+    A.Normalize(mean=(0.4611, 0.4403, 0.4193), std=(0.2107, 0.2074, 0.2157)),
+    ToTensorV2(),
+]
+
+# _test_transform = [ToTensorV2()]
+_test_transform = [
+    A.Resize(256, 256),
+    A.Normalize(mean=(0.4611, 0.4403, 0.4193), std=(0.2107, 0.2074, 0.2157)),
+    ToTensorV2(),
+]
 def get_classname(classID, cats):
     for i in range(len(cats)):
         if cats[i]["id"] == classID:
             return cats[i]["name"]
     return "None"
 
-
+@copy_paste_class
 class CustomDataLoader(Dataset):
     """COCO format"""
-
     category_names = [
         "Backgroud",
         "UNKNOWN",
@@ -65,25 +112,33 @@ class CustomDataLoader(Dataset):
         "Battery",
         "Clothing",
     ]
-
-    def __init__(self, data_dir, mode="train", transform=None):
+    def __init__(self, data_dir, mode = 'train', transform = None):
         super().__init__()
         self.mode = mode
-        self.transform = transform
+        self.transforms = transform
         self.coco = COCO(data_dir)
+        
 
-    def __getitem__(self, index: int):
+    def load_example(self, index: int):
+        """__getitem__ 함수라고 생각
+
+        Args:
+            index (int)
+
+        Returns:
+            [dict]]: [image 정보(images,masks,boxex)]
+        """
         # dataset이 index되어 list처럼 동작
         image_id = self.coco.getImgIds(imgIds=index)
         image_infos = self.coco.loadImgs(image_id)[0]
-
+        
         # cv2 를 활용하여 image 불러오기
-        images = cv2.imread(os.path.join(dataset_path, image_infos["file_name"]))
+        images = cv2.imread(os.path.join(dataset_path, image_infos['file_name']))
         images = cv2.cvtColor(images, cv2.COLOR_BGR2RGB).astype(np.float32)
-        images /= 255.0
-
-        if self.mode in ("train", "val"):
-            ann_ids = self.coco.getAnnIds(imgIds=image_infos["id"])
+        # images /= 255.0
+        
+        if (self.mode in ('train', 'val')):
+            ann_ids = self.coco.getAnnIds(imgIds=image_infos['id'])
             anns = self.coco.loadAnns(ann_ids)
 
             # Load the categories in a variable
@@ -95,31 +150,109 @@ class CustomDataLoader(Dataset):
             # Background = 0
             masks = np.zeros((image_infos["height"], image_infos["width"]))
             # Unknown = 1, General trash = 2, ... , Cigarette = 11
+            bboxes=[]
+            maskses=[]
             for i in range(len(anns)):
-                className = get_classname(anns[i]["category_id"], cats)
+                className = get_classname(anns[i]['category_id'], cats)
+                # print(className)
                 pixel_value = self.category_names.index(className)
-                masks = np.maximum(self.coco.annToMask(anns[i]) * pixel_value, masks)
+                # masks = np.maximum(self.coco.annToMask(anns[i])*pixel_value, masks)
+                maskses.append(np.maximum(self.coco.annToMask(anns[i])*pixel_value, masks))
+                # masks.append(self.coco.annToMask(obj))
+                bboxes.append(anns[i]['bbox']+[anns[i]['category_id']]+[i])
             masks = masks.astype(np.float32)
-
+            output = {
+            'image': images,
+            'masks': maskses,
+            'bboxes': bboxes
+            }   
+            # return images, masks, bboxes,image_infos
+            return self.transforms(**output)
+            # return images, masks
+        
+        if self.mode == 'test':
             # transform -> albumentations 라이브러리 활용
-            if self.transform is not None:
-                transformed = self.transform(image=images, mask=masks)
+            if self.transforms is not None:
+                transformed = self.transforms(image=images)
                 images = transformed["image"]
-                masks = transformed["mask"]
-
-            return images, masks, image_infos
-
-        if self.mode == "test":
-            # transform -> albumentations 라이브러리 활용
-            if self.transform is not None:
-                transformed = self.transform(image=images)
-                images = transformed["image"]
-
+            
             return images, image_infos
 
-    def __len__(self):
+    def __len__(self) -> int:
         # 전체 dataset의 size를 return
         return len(self.coco.getImgIds())
+# class CustomDataLoader(Dataset):
+#     """COCO format"""
+
+#     category_names = [
+#         "Backgroud",
+#         "UNKNOWN",
+#         "General trash",
+#         "Paper",
+#         "Paper pack",
+#         "Metal",
+#         "Glass",
+#         "Plastic",
+#         "Styrofoam",
+#         "Plastic bag",
+#         "Battery",
+#         "Clothing",
+#     ]
+
+#     def __init__(self, data_dir, mode="train", transform=None):
+#         super().__init__()
+#         self.mode = mode
+#         self.transform = transform
+#         self.coco = COCO(data_dir)
+
+#     def __getitem__(self, index: int):
+#         # dataset이 index되어 list처럼 동작
+#         image_id = self.coco.getImgIds(imgIds=index)
+#         image_infos = self.coco.loadImgs(image_id)[0]
+
+#         # cv2 를 활용하여 image 불러오기
+#         images = cv2.imread(os.path.join(dataset_path, image_infos["file_name"]))
+#         images = cv2.cvtColor(images, cv2.COLOR_BGR2RGB).astype(np.float32)
+#         # images /= 255.0
+
+#         if self.mode in ("train", "val"):
+#             ann_ids = self.coco.getAnnIds(imgIds=image_infos["id"])
+#             anns = self.coco.loadAnns(ann_ids)
+
+#             # Load the categories in a variable
+#             cat_ids = self.coco.getCatIds()
+#             cats = self.coco.loadCats(cat_ids)
+
+#             # masks : size가 (height x width)인 2D
+#             # 각각의 pixel 값에는 "category id + 1" 할당
+#             # Background = 0
+#             masks = np.zeros((image_infos["height"], image_infos["width"]))
+#             # Unknown = 1, General trash = 2, ... , Cigarette = 11
+#             for i in range(len(anns)):
+#                 className = get_classname(anns[i]["category_id"], cats)
+#                 pixel_value = self.category_names.index(className)
+#                 masks = np.maximum(self.coco.annToMask(anns[i]) * pixel_value, masks)
+#             masks = masks.astype(np.float32)
+
+#             # transform -> albumentations 라이브러리 활용
+#             if self.transform is not None:
+#                 transformed = self.transform(image=images, mask=masks)
+#                 images = transformed["image"]
+#                 masks = transformed["mask"]
+
+#             return images, masks, image_infos
+
+#         if self.mode == "test":
+#             # transform -> albumentations 라이브러리 활용
+#             if self.transform is not None:
+#                 transformed = self.transform(image=images)
+#                 images = transformed["image"]
+
+#             return images, image_infos
+
+#     def __len__(self):
+#         # 전체 dataset의 size를 return
+#         return len(self.coco.getImgIds())
 
 
 # collate_fn needs for batch
@@ -127,57 +260,69 @@ def collate_fn(batch):
     return tuple(zip(*batch))
 
 
-def get_dataloader(batch_size = 20):
-    train_transform = A.Compose([
-        A.Resize(256,256),
-        ToTensorV2()])
+def get_dataloader(batch_size=20, fold_index=None):
+    """dataloader를 반환합니다
 
-    val_transform = A.Compose([
-        A.Resize(256,256),
-        ToTensorV2()])
+    Args:
+        batch_size (int) : Defaults to 20.
+        fold_index (tuple): train_index와 val_index
 
-    # create own Dataset 1 (skip)
-    # validation set을 직접 나누고 싶은 경우
-    # random_split 사용하여 data set을 8:2 로 분할
-    # train_size = int(0.8*len(dataset))
-    # val_size = int(len(dataset)-train_size)
-    # dataset = CustomDataLoader(data_dir=train_path, mode='train', transform=transform)
-    # train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+    Returns:
+        (train_loader, val_loader)
+    """
+    train_transform = A.Compose(_train_transform, bbox_params=A.BboxParams(format="coco", min_visibility=0.05))
 
-    # create own Dataset 2
-    # train dataset
-    train_dataset = CustomDataLoader(
-        data_dir=train_path, mode="train", transform=train_transform
-    )
+    val_transform = A.Compose(_valid_transform)
 
-    # validation dataset
-    val_dataset = CustomDataLoader(
-        data_dir=val_path, mode="val", transform=val_transform
-    )
+    if fold_index:
+        # Kfold
+        dataset = CustomDataLoader(data_dir=all_path, mode='train', transform=train_transform)
 
-    # DataLoader
-    train_loader = torch.utils.data.DataLoader(
-        dataset=train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=4,
-        collate_fn=collate_fn,
-    )
+        train_subsampler = SubsetRandomSampler(fold_index[0]) # train_index sampler
+        val_subsampler = SubsetRandomSampler(fold_index[1]) # val_index sampler
 
-    val_loader = torch.utils.data.DataLoader(
-        dataset=val_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=4,
-        collate_fn=collate_fn,
-    )
+        train_loader = DataLoader(
+            dataset, batch_size=batch_size, sampler=train_subsampler,
+            num_workers=4, collate_fn=collate_fn, drop_last=True)
+        val_loader = DataLoader(
+            dataset, batch_size=batch_size, sampler=val_subsampler, 
+            num_workers=4, collate_fn=collate_fn, drop_last=True)
+
+    else:
+        # Hold Out
+        # train dataset
+        train_dataset = CustomDataLoader(
+            data_dir=train_path, mode="train", transform=train_transform
+        )
+
+        # validation dataset
+        val_dataset = CustomDataLoader(
+            data_dir=val_path, mode="val", transform=val_transform
+        )
+
+        # DataLoader
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=4,
+            collate_fn=collate_fn,
+        )
+
+        val_loader = torch.utils.data.DataLoader(
+            dataset=val_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=4,
+            collate_fn=collate_fn,
+        )
 
     return train_loader, val_loader
 
 
 def get_testloader(batch_size = 20):
-    test_transform = A.Compose([A.Resize(256,256),ToTensorV2()])
-
+    # test_transform = A.Compose([A.Resize(256,256),ToTensorV2()])
+    test_transform = A.Compose(_test_transform)
     # test dataset
     test_dataset = CustomDataLoader(
         data_dir=test_path, mode="test", transform=test_transform
--- experiment/jaehyuk/code/train.py
+++ experiment/jaehyuk/code/train.py
@@ -3,7 +3,7 @@ import random
 import time
 
 import wandb
-
+import copy
 import numpy as np
 import torch
 from torch import nn, optim
@@ -17,9 +17,9 @@ from scheduler import create_scheduler
 from model import get_model
 from utils import seed_everything, label_accuracy_score, add_hist
 from evaluation import save_model
-
+from sklearn.model_selection import KFold
 WANDB = True
-WANDB = False
+# WANDB = False
 
 def train(args,epoch,num_epochs, model, criterions, optimizer, dataloader,scheduler=None):
     model.train()
@@ -27,28 +27,58 @@ def train(args,epoch,num_epochs, model, criterions, optimizer, dataloader,schedu
     # labels = torch.tensor([]).to(args.device)
     # preds = torch.tensor([]).to(args.device)
 
-    for step,(images, masks, _) in enumerate(dataloader) :
+    for step,image_data in enumerate(dataloader) :
         optimizer.zero_grad()
-
+        # image_data = dict.values 형태  
+        # image_data[0]=image -> Tuple  len(image) = batch  Tuple[0] ->Tensor (channel,height,width)
+        # image_data[1]=mask -> Tuple   len(mask) = batch  Tuple[0] ->Tensor (mask_length,height,width)
+        # image_data[2]=bbox -> Tuple   len(bbox) = batch  Tuple[0] ->Tensor (bbox_length,height,width)
+        images=image_data[0]
         images = torch.stack(images)       # (batch, channel, height, width)
-        masks = torch.stack(masks).long()  # (batch, channel, height, width)
+
+        temp_masks = image_data[1]
+        # masks = np.zeros(images.shape[2:])
+        masks=[]
+        # 배치 사이즈 만큼 for문 
+        for temp_mask in temp_masks:
+            temp= np.zeros(images.shape[2:]) # width, height 크기만큼의 0 array 생성
+            # 클래스 개수(12) 만큼 for문
+            for mask in temp_mask:
+                temp=np.maximum(mask, temp)
+            masks.append(temp)
+        masks=torch.tensor(masks).long()
+        # masks = torch.stack(masks).long()  # (batch, channel, height, width)
         
         images, masks = images.to(args.device), masks.to(args.device)
-                
-        outputs = model(images)
-        flag=criterions[0]
-        if flag=='+':
-            loss = criterions[1](outputs, masks)+ criterions[2](outputs, masks)
-        elif flag=='-':
-            loss = criterions[1](outputs, masks) - criterions[2](outputs, masks)
+        
+        # 확률에 따라 cutmix 사용 , cutmix에서는 로스 조합 x
+        if np.random.rand(1)<0.5:
+            lam = np.random.beta(1.0, 1.0)
+            rand_index = torch.randperm(images.size()[0]).cuda()
+            target_a = masks
+            target_b = masks[rand_index]
+            bbx1, bby1, bbx2, bby2 = rand_bbox(images.size(), lam)
+            images[:, :, bbx1:bbx2, bby1:bby2] = images[rand_index, :, bbx1:bbx2, bby1:bby2]
+            # adjust lambda to exactly match pixel ratio
+            lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (images.size()[-1] * images.size()[-2]))
+            # compute output
+            outputs = model(images)
+            loss = criterions[1](outputs, target_a) * lam + criterions[1](outputs, target_b) * (1. - lam)
         else:
-            loss = criterions[1](outputs, masks)
+            outputs = model(images)
+            flag=criterions[0]
+            if flag=='+':
+                loss = criterions[1](outputs, masks)+ criterions[2](outputs, masks)
+            elif flag=='-':
+                loss = criterions[1](outputs, masks) - criterions[2](outputs, masks)
+            else:
+                loss = criterions[1](outputs, masks)
         # loss = criterion(outputs, masks)
 
         loss.backward()
 
         optimizer.step()
-        if (step + 1) % 25 == 0:
+        if (step + 1) % 10 == 0:
             current_lr = get_lr(optimizer)
             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f} lr: {}'.format(
                     epoch+1, num_epochs, step+1, len(dataloader), loss.item(),current_lr))
@@ -64,10 +94,25 @@ def evaluate(args, model, criterions, dataloader):
     n_class = 12
     with torch.no_grad():
         hist = np.zeros((n_class, n_class))
-        for images, masks, _ in dataloader:
-            
+        for image_data in dataloader:
+
+            images=image_data[0]
             images = torch.stack(images)       # (batch, channel, height, width)
-            masks = torch.stack(masks).long()  # (batch, channel, height, width)
+
+            temp_masks = image_data[1]
+            # masks = np.zeros(images.shape[2:])
+            masks=[]
+            # 배치 사이즈 만큼 for문 
+            for temp_mask in temp_masks:
+                temp= np.zeros(images.shape[2:]) # width, height 크기만큼의 0 array 생성
+                # 클래스 개수(12) 만큼 for문
+                for mask in temp_mask:
+                    temp=np.maximum(mask, temp)
+                masks.append(temp)
+            masks=torch.tensor(masks).long()
+
+            # images = torch.stack(images)       # (batch, channel, height, width)
+            # masks = torch.stack(masks).long()  # (batch, channel, height, width)
 
             images, masks = images.to(args.device), masks.to(args.device)            
 
@@ -122,49 +167,101 @@ def run(args, model, criterion, optimizer, dataloader,scheduler=None):
 
 def main(args):
     seed_everything(21)
+    # load_dotenv()
+
     if WANDB:
-        wandb.init(project="stage-3", reinit=True)
         if args.ENCODER:
-            wandb.run.name = args.MODEL +'_' +args.ENCODER +'_' +args.FILE_NAME
+            run_name = args.MODEL +'_' +args.ENCODER
         else:
-            wandb.run.name = args.MODEL +'_' +args.FILE_NAME
-        wandb.config.update(args)
-
-        args = wandb.config
-
-    dataloader = get_dataloader(args.BATCH_SIZE)
-    print("Get loader")
-
-    model = get_model(args.MODEL,args.ENCODER).to(args.device)
-    print("Load model")
-
-    if WANDB:
-        wandb.watch(model)
+            run_name = args.MODEL
+            
     
-    criterion=[]
-    if '+' in args.LOSS :
-        criterion.append('+')
-        criterion.append(create_criterion(args.LOSS.split('+')[0]))
-        criterion.append(create_criterion(args.LOSS.split('+')[1]))        
-    elif '-' in args.LOSS:
-        criterion.append('-')
-        criterion.append(create_criterion(args.LOSS.split('-')[0]))
-        criterion.append(create_criterion(args.LOSS.split('-')[1]))   
-    else:
-        criterion.append('0')
-        criterion.append(create_criterion(args.LOSS))
-    optimizer = create_optimizer(args.OPTIMIZER,model,args.LEARNING_RATE)
-    if args.SCHEDULER:
-        scheduler = create_scheduler(args.SCHEDULER,optimizer)
-    else:
-        scheduler = None
-    # optimizer = optim.Adam(params = model.parameters(), lr = args.LEARNING_RATE, weight_decay=1e-6)
+    if args.KFOLD > 1:
+        # kfold index 생성
+        fold_split = KFold(args.KFOLD, shuffle=True, random_state=21)
+        index_gen = iter(fold_split.split(range(3272))) # 전체 이미지수 3272개
+        # pt 저장 폴더 생성
+        path_pair = args.MODEL_PATH.split('.')
+        os.makedirs(path_pair[0], exist_ok=True) 
+        # 재사용위해 args 복사
+        args_origin = copy.deepcopy(args) 
+        
+    for i in range(args.KFOLD):
+        # hold-out, kfold에 따라서 dataloader 다르게 설정
+        if args.KFOLD > 1:
+            # wandb
+            if WANDB:
+                args = copy.deepcopy(args_origin)
+                path_pair = args_origin.MODEL_PATH.split('.')
+                args.MODEL_PATH = path_pair[0] + f'/kfold_{i+1}.' + path_pair[1] # MODEL_PATH 변경
+                wandb.init(project=os.environ.get('WANDB_PROJECT_NAME'), name=run_name+f'_k{i+1}', config=args, reinit=True)
+                args = wandb.config
+            # dataloader
+            dataloader = get_dataloader(args.BATCH_SIZE, fold_index=next(index_gen))
+            print(f'\nfold {i+1} start')
+        else:
+            # wandb
+            if WANDB:
+                wandb.init(project=os.environ.get('WANDB_PROJECT_NAME'), name=run_name, reinit=True)
+                wandb.config.update(args)
+                args = wandb.config
+            # dataloader
+            dataloader = get_dataloader(args.BATCH_SIZE)
+        print("Get loader")
+
+        model = get_model(args.MODEL, args.ENCODER).to(args.device)
+        print("Load model")
+
+        if WANDB:
+            wandb.watch(model)
     
-    print("Run")
-    run(args, model, criterion, optimizer ,dataloader,scheduler)
+        criterion=[]
+        if '+' in args.LOSS :
+            criterion.append('+')
+            criterion.append(create_criterion(args.LOSS.split('+')[0]))
+            criterion.append(create_criterion(args.LOSS.split('+')[1]))        
+        elif '-' in args.LOSS:
+            criterion.append('-')
+            criterion.append(create_criterion(args.LOSS.split('-')[0]))
+            criterion.append(create_criterion(args.LOSS.split('-')[1]))   
+        else:
+            criterion.append('0')
+            criterion.append(create_criterion(args.LOSS))
+            
+        if i%2==0 :
+            optimizer = create_optimizer('AdamP',model,args.LEARNING_RATE)
+        else:
+            optimizer = create_optimizer('SGDP',model,args.LEARNING_RATE)
+        
+        print(optimizer)
+        if args.SCHEDULER:
+            scheduler = create_scheduler(args.SCHEDULER,optimizer)
+        else:
+            scheduler = None
+        # optimizer = optim.Adam(params = model.parameters(), lr = args.LEARNING_RATE, weight_decay=1e-6)
+        
+        print("Run")
+        run(args, model, criterion, optimizer ,dataloader,scheduler)
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
+
+def rand_bbox(size,lam):
+    H=size[2]
+    W=size[3]
+    cut_rat=np.sqrt(1.-lam)
+    cut_w=np.int(W*cut_rat)
+    cut_h=np.int(H*cut_rat)
+
+    cx=np.random.randint(W)
+    cy=np.random.randint(H)
+
+    bbx1=np.clip(cx-cut_w//2,0,W)
+    bby1=np.clip(cy-cut_h//2,0,H)
+    bbx2=np.clip(cx+cut_w//2,0,W)
+    bby2=np.clip(cy+cut_h//2,0,H)
+
+    return bbx1, bby1,bbx2, bby2
 if __name__ == "__main__":
     args = get_args()
     torch.cuda.empty_cache()
